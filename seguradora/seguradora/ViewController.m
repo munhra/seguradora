d1838c2c36c342ebd3ca93617ea50b07bfd9bbfb
Rafael Munhoz <rafael.munhoz@gmail.com>
2014-12-13 10:44:41 -0200

    First Commit

---
+++ seguradora/seguradora/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  seguradora
+//
+//  Created by Rafael Munhoz on 13/12/14.
+//  Copyright (c) 2014 Rafael Munhoz. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ seguradora/seguradora/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  seguradora
+//
+//  Created by Rafael Munhoz on 13/12/14.
+//  Copyright (c) 2014 Rafael Munhoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
