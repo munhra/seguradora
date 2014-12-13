18d03f9a836156d3e8fb8ca2756fecfc893c9342
Rafael Munhoz <rafael.munhoz@gmail.com>
2014-12-13 12:10:09 -0200

    Partial Navigation

---
+++ seguradora/seguradora/CotacaoViewCtrl.h
@@ -0,0 +1,13 @@
+//
+//  CotacaoViewCtrl.h
+//  seguradora
+//
+//  Created by Rafael Munhoz on 13/12/14.
+//  Copyright (c) 2014 Rafael Munhoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CotacaoViewCtrl : UIViewController
+
+@end
---
+++ seguradora/seguradora/SegurosTabViewCtrl.h
@@ -0,0 +1,13 @@
+//
+//  SegurosTabViewCtrl.h
+//  seguradora
+//
+//  Created by Rafael Munhoz on 13/12/14.
+//  Copyright (c) 2014 Rafael Munhoz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SegurosTabViewCtrl : UITableViewController
+
+@end
